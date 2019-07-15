bf0bae4cca4cd4ea31ff45ced6e5fe51a070c40d
Dylan Crawshaw <dcrawsh@gmail.com>
2019-07-15 23:26:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+counter = 0
+ 
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+    array **2
 end
+
+numbers = [1,2,3]
+
+square_array(numbers)
+
